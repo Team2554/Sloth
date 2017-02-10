1045af4a20204bd8e74dc27f3c3ce34cb385291b
Kevin Yang <creepermania9@live.com>
2017-02-10 17:50:16 -0500

    Recovered Deleted Code

--- src/org/usfirst/frc/team2554/robot/OI.java
+++ src/org/usfirst/frc/team2554/robot/OI.java
@@ -3,7 +3,6 @@ package org.usfirst.frc.team2554.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 
-import org.usfirst.frc.team2554.robot.commands.DriveTrainDefault;
 
 /**
  * This class is the glue that binds the controls on the physical operator
---
+++ src/org/usfirst/frc/team2554/robot/commands/SpinShooter.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team2554.robot.commands;
+
+import org.usfirst.frc.team2554.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class SpinShooter extends Command {
+
+    public SpinShooter() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.shooter);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.shooter.spin();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	if(Robot.oi.controller.getRawAxis(Robot.oi.ryAxis) == 0)
+    		return true;
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.shooter.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
