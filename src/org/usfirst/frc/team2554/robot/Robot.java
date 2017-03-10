cff903bca98c8f5c217e17a96f3ccdd794654429
Arjun Srivastav <arjun.stv@gmail.com>
2017-03-10 09:04:29 -0500

    fixed issue #9

--- src/org/usfirst/frc/team2554/robot/Robot.java
+++ src/org/usfirst/frc/team2554/robot/Robot.java
@@ -149,7 +149,6 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void teleopInit() {
-		gyro.calibrate();
 		// This makes sure that the autonomous stops running when
 		// teleop starts running. If you want the autonomous to
 		// continue until interrupted by another command, remove
